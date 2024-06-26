a359103382d5c3c70af58e0bc5d314f2ca87c4b1
云 <zy.kumo@gmail.com>
2024-06-25 21:43:18 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -163,7 +163,7 @@ const config = {
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Methods", value: "*" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
       }
