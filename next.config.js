0eb35b9a73df9b4bca9483be2306d63ab2967f7b
云 <zy.kumo@gmail.com>
2024-06-25 21:51:15 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -36,6 +36,10 @@ const headers = [
       .replace(/\s{2,}/g, ' ')
       .trim(),
   },
+  { key: "Access-Control-Allow-Credentials", value: "true" },
+  { key: "Access-Control-Allow-Origin", value: "*" },
+  { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+  { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
 ];
 
 if (forceSSL) {
@@ -157,15 +161,6 @@ const config = {
       {
         source: '/:path*',
         headers,
-      },
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "*" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
       }
     ];
   },
